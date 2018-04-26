539c8bf105e8f921639cbcd6bf5723d2badf3495
Anders Kalhauge <anders@kalhauge.dk>
2018-04-26 15:10:50 +0200

    .gitignore added comment on MCChecker

--- src/main/kotlin/dk/edutor/eduport/mc/MCChecker.kt
+++ src/main/kotlin/dk/edutor/eduport/mc/MCChecker.kt
@@ -7,6 +7,9 @@ class MCChecker : Port {
     override fun check(challenge: Challenge, solution: Solution): Assessment {
         if (!(challenge is MCChallenge)) throw IllegalArgumentException("Challenge should be Multiple Choice")
         if (!(solution is MCSolution)) throw IllegalArgumentException("Solution should be Multiple Choice")
+//        return Assessment(solution,
+//            if (solution.answers.all { challenge.answers[it] ?: false }) 12 else 0
+//            )
         try {
             for (answer: String in solution.answers) {
                 check(challenge.answers[answer]!!)
